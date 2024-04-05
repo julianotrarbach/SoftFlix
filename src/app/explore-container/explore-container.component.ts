1a3e68eee87850762e930d3ceaea0fbf154cb839
Juliano Mello Trarbach <julianotrarbach@bne.com.br>
2024-04-05 20:22:13 +0100

    alerts

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { MovieModel } from 'src/models/movie.model';
 import { LocalStorageService } from 'src/services/local-storage.service';
 
@@ -11,7 +12,10 @@ export class ExploreContainerComponent {
 
   @Input() movies?: Array<MovieModel>;
 
-  constructor(private localStorageService: LocalStorageService) { }
+  constructor(
+    private localStorageService: LocalStorageService,
+    private alertController: AlertController
+  ) { }
 
   addWatched(data: any): void {
 
@@ -20,9 +24,11 @@ export class ExploreContainerComponent {
     const indexToRemove = list?.findIndex(m => m.imdbID === data.imdbID);
     if (indexToRemove != null && indexToRemove !== -1) {
       list.splice(indexToRemove, 1);
+      this.sucessMessage("Done");
     } else {
       if (!list) { list = [] };
       list.push(data);
+      this.sucessMessage("Done");
     }
 
     this.localStorageService.setItem("watchedList", list);
@@ -34,12 +40,50 @@ export class ExploreContainerComponent {
     const indexToRemove = list?.findIndex(m => m.imdbID === data.imdbID);
     if (indexToRemove != null && indexToRemove !== -1) {
       list.splice(indexToRemove, 1);
+      this.sucessMessage("Done");
     } else {
       if (!list) { list = [] };
       list.push(data);
+      this.sucessMessage("Done");
     }
 
     this.localStorageService.setItem("myList", list);
   }
+
+  async sucessMessage(message: string) {
+    const alert = await this.alertController.create({
+      message: message
+    });
+
+    await alert.present();
+  }
+
+  async confirmMessageWatched(movie: MovieModel) {
+
+    const alert = await this.alertController.create({
+      header: '',
+      subHeader: '',
+      message: 'Are you sure in remove this one of Watched list?',
+      buttons: [
+        {
+          text: 'Sim',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            this.addWatched(movie);
+          },
+        },
+        {
+          text: 'Não',
+          handler: () => {
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+
 
 }
